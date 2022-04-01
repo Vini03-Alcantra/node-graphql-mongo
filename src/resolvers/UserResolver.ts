f908f259efa22604b5e16c7503db801789483e22
Vinícius Alcântra <viniciusalcantarabarros@gmail.com>
2022-03-31 22:48:30 -0300

    Add mutations and queryes

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,11 +1,24 @@
-import {Query, Resolver} from "type-graphql";
-
+import {Arg, Mutation, Query, Resolver} from "type-graphql";
+import crypto from "crypto"
+import {User} from "../models/User"
 @Resolver()
 export class UserResolve {
-    
-    @Query(() => String)
+    private data: User[] = [];
+
+    @Query(() => [User])
     async users(){
-        return "Hello World"
+        return this.data
+    }
+
+    @Mutation(() => User)
+    async createUser(
+        @Arg('name') name: string
+    ) {
+        const user = {id: crypto.randomUUID(), name}
+
+        this.data.push(user)
+
+        return user
     }
 }
 
